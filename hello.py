021ddb23d4030c8bf42917aeef8bc8f3900270b2
rocklym <rocklymp@gmail.com>
2017-04-13 00:44:32 -0700

    update

--- hello.py
+++ hello.py
@@ -1,20 +1,38 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
-from flask_script import Manager
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
+from threading import Thread
+import os
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "benben"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rock:11111@localhost/pixie_dev'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.163.com'
+app.config['MAIL_PORT'] = 25
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') or 'rocklym@163.com'
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') or 'abc123'
+app.config['PIXIE_MAIL_SUBJECT_PREFIX'] = '[PIXIE]'
+app.config['PIXIE_MAIL_SENDER'] = 'Pixie Admin <rocklym@163.com>'
+app.config['PIXIE_ADMIN'] = os.environ.get('PIXIE_ADMIN') or 'rocklym@163.com'
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
+migrate = Migrate(app, db)
 
-app.config['SECRET_KEY'] = "benben is chou benben"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rock:11111@localhost/pixie_dev'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 class NameForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
@@ -41,16 +59,38 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['PIXIE_MAIL_SUBJECT_PREFIX'] + subject,
+                  sender=app.config['PIXIE_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['PIXIE_ADMIN']:
+                send_email(app.config['PIXIE_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<name>')
@@ -67,6 +107,13 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html')
 
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
