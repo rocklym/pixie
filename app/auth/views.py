d07f7eb052c311306e43d906babea756948c1526
rocklym <rocklymp@gmail.com>
2017-04-19 23:10:23 -0700

    20170420.1

--- app/auth/views.py
+++ app/auth/views.py
@@ -105,8 +105,11 @@ def password_reset_request():
             token = user.generate_reset_token()
             send_email(user.email, 'Reset Your Password', 'auth/email/reset_password', user=user, token=token,
                        next=request.args.get('next'))
-        flash('A email with instructions to reset your password has been sent to you')
-        return redirect(url_for('auth.login'))
+            flash('A email with instructions to reset your password has been sent to you')
+            return redirect(url_for('auth.login'))
+        else:
+            flash("Email doesn't exist.")
+            return redirect(url_for('auth.password_reset_request'))
     return render_template('auth/reset_password.html', form=form)
 
 
